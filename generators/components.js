2762cc6ce2a05d8b91eb15dbc4da16f0074c86b2
Williams25 <william.souza.timao@hotmail.com>
2022-06-20 21:05:03 -0300

    adicionando configurações de estruturas do projeto

---
+++ generators/components.js
@@ -0,0 +1,23 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "application controller",
+
+    // inquirer prompts
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "component name?",
+      },
+    ],
+
+    // actions to perform
+    actions: [
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "../templates/components/index.tsx.hbs",
+      },
+    ],
+  });
+};
---
+++ generators/pages.js
@@ -0,0 +1,23 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "application controller",
+
+    // inquirer prompts
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "page name?",
+      },
+    ],
+
+    // actions to perform
+    actions: [
+      {
+        type: "add",
+        path: "../src/layouts/{{pascalCase name}}/index.tsx",
+        templateFile: "../templates/pages/index.tsx.hbs",
+      },
+    ],
+  });
+};
