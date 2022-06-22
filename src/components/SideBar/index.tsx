aca3c1f41d40ff4beed5320b49557ba3824eb3e1
Williams25 <william.souza.timao@hotmail.com>
2022-06-21 21:21:14 -0300

    criando sidebar e adicionando chamada para listar as aulas e adicionando loading

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@apollo/client";
+import { Spinner } from "phosphor-react";
+import { GET_LESSONS } from "../../graphql/queries/GET_LESSONS";
+import { GetLessonsQueryResponse } from "../../types/GetLessonsQueryResponse";
+import { Lesson } from "../";
+
+export const SideBar = () => {
+  const { data: lessons, loading } = useQuery<{
+    lessons: GetLessonsQueryResponse[];
+  }>(GET_LESSONS);
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600 overflow-hidden">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas {String(loading)}
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {!loading &&
+          lessons?.lessons.map((item) => (
+            <Lesson
+              key={item.id}
+              title={item.title}
+              slug={item.slug}
+              type={item.lessonType}
+              avaliableAt={new Date(item.availableAt)}
+            />
+          ))}
+
+        {loading && (
+          <div className="w-full h-[50vh] flex items-center justify-center animate-spin">
+            <Spinner size={30} />
+          </div>
+        )}
+      </div>
+    </aside>
+  );
+};
