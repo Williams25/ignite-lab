2010fa300c10b63f72497a77411329ccc5a58aca
Williams25 <william.souza.timao@hotmail.com>
2022-06-21 21:29:24 -0300

    removendo texto do titulo da sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -12,7 +12,7 @@ export const SideBar = () => {
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600 overflow-hidden">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
-        Cronograma de aulas {String(loading)}
+        Cronograma de aulas
       </span>
 
       <div className="flex flex-col gap-8">
