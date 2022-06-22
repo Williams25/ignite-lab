f1d4782cab913c0868533c13dc38bb91294293ab
Williams25 <william.souza.timao@hotmail.com>
2022-06-21 21:25:11 -0300

    renderizando pagina de evento

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useQuery } from "@apollo/client";
-import { GET_LESSONS } from "./graphql/queries/GET_LESSONS";
-import { Lesson } from "types/Lesson";
+import { Event } from "./pages/Event";
 
 export const App = () => {
-  const { data: lessons } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS);
-
   return (
-    <div>
-      <ul>
-        {lessons?.lessons.map((lesson, key) => (
-          <li key={key}>{lesson.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Event />
+    </>
   );
 };
