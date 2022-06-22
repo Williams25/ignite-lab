71ff295b1925d3d8732ed2af6ae90e76a86b3c7c
Williams25 <william.souza.timao@hotmail.com>
2022-06-21 21:23:31 -0300

    conficurandoo font e colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif"
+      },
+      colors: {
+        gray: {
+          100: "#e1e1e6",
+          200: "#c4c4cc",
+          300: "#8d8d99",
+          500: "#323238",
+          600: "#28282e",
+          700: "#121214",
+          900: "#09090a"
+        },
+        red: {
+          500: "#f75a68"
+        },
+        orange: {
+          500: "#fba94c"
+        },
+        blue: {
+          500: "#81d8f7"
+        },
+        green: {
+          300: "#00b37e",
+          500: "#00875f",
+          700: "#015f43"
+        }
+      }
+    }
   },
   plugins: [require("@tailwindcss/forms"), require("tailwindcss-scrollbar")]
 };
