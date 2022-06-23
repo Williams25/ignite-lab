ff678f3358ea5630f3bb67ea2b7e2db318e639ea
Williams25 <william.souza.timao@hotmail.com>
2022-06-22 21:26:11 -0300

    adicionando link para a aula e adicionando animação de hover

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { Link } from "react-router-dom";
 
 export type LessonProps = {
   title: string;
@@ -13,20 +14,24 @@ export const Lesson = ({ avaliableAt, slug, title, type }: LessonProps) => {
   const isLessonAvaliable = isPast(avaliableAt);
   const availableDateFormated = format(
     avaliableAt,
-    "EEEE' • 'dd' de 'MMMM' • 'K'h'mm",
+    "EEEE' • 'dd' de 'MMMM' • 'k'h'mm",
     {
       locale: ptBR
     }
   );
 
   return (
-    <a
-      href={slug}
-      className={`${isLessonAvaliable ? "" : "opacity-50 cursor-no-drop"}`}
+    <Link
+      to={`/event/lesson/${slug}`}
+      className={`${
+        isLessonAvaliable
+          ? "group"
+          : "opacity-50 cursor-no-drop pointer-events-none select-none"
+      }`}
     >
       <span className="text-gray-300">{availableDateFormated}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
         <header className="flex items-center justify-between">
           {isLessonAvaliable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -45,6 +50,6 @@ export const Lesson = ({ avaliableAt, slug, title, type }: LessonProps) => {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 };
