a770c0ddb79deeebc8788f1fc07ff0391d47fcd3
Williams25 <william.souza.timao@hotmail.com>
2022-06-24 20:11:29 -0300

    criando queries e mutations

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o2btdg05og01xr3q642hcs/master",
   cache: new InMemoryCache(),
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
--- src/graphql/queries/CREATE_SUBSCRIBER.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const CREATE_SUBSCRIBER = gql`
-  mutation CREATE_SUBSCRIBER($name: String!, $email: String!) {
-    createSubscriber(data: { name: $name, email: $email }) {
-      id
-    }
-  }
-`;
